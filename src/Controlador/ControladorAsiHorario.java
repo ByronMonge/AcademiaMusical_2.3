57d1fb184665a82b9b50f4d4f51ccbbbb2beb082
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-07 15:42:32 -0500

    No tocar

---
+++ src/Controlador/ControladorAsiHorario.java
@@ -0,0 +1,137 @@
+package Controlador;
+
+import Modelo.Curso;
+import Modelo.ModeloAsiHorario;
+import Modelo.ModeloCurso;
+import Vista.VistaAsiHorario;
+import Vista.VistaPrincipal;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.xml.ws.Holder;
+
+public class ControladorAsiHorario {
+
+    ModeloAsiHorario modelo;
+    VistaAsiHorario vista;
+
+    VistaPrincipal p = new VistaPrincipal();
+
+    public ControladorAsiHorario(ModeloAsiHorario modelo, VistaAsiHorario vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+        vista.setVisible(true);
+        vista.setSize(p.getEscritorioPrincipal().getWidth(), p.getEscritorioPrincipal().getHeight());
+    }
+
+    public void iniciarControl() {
+
+    }
+
+    //Todo sobre el registro de cursos en el jDialog
+    public void abrirjDialogCurso() {
+        vista.getjDlgBuscarCurso().setLocationRelativeTo(null);
+        vista.getjDlgBuscarCurso().setSize(476, 388);
+        vista.getjDlgBuscarCurso().setTitle("Seleccione un curso");
+        vista.getjDlgBuscarCurso().setVisible(true);
+        cargarRegistroDeCursos();
+        buscarCurso();
+    }
+
+    public void cargarRegistroDeCursos() {
+
+        ModeloCurso modeloCurso = new ModeloCurso();
+        vista.getTblDlgjCurso().setRowHeight(25);
+        DefaultTableModel estructuraTabla = (DefaultTableModel) vista.getTblDlgjCurso().getModel();
+        estructuraTabla.setRowCount(0);
+
+        List<Curso> listap = modeloCurso.listaCursoTabla();
+
+        Holder<Integer> i = new Holder<>(0);
+
+        listap.stream().forEach(c -> {
+
+            estructuraTabla.addRow(new Object[8]);
+
+            vista.getTblDlgjCurso().setValueAt(c.getCur_codigo(), i.value, 0);
+            vista.getTblDlgjCurso().setValueAt(c.getCur_nombre(), i.value, 1);
+            vista.getTblDlgjCurso().setValueAt(c.getCur_periodo(), i.value, 2);
+
+            i.value++;
+        });
+    }
+
+    public void cargarDatosCursoEnTXT() {
+        int fila = vista.getTblDlgjCurso().getSelectedRow();
+
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(null, "Aun no ha seleccionado una fila");
+        } else {
+
+            //Quito la visibilidad del txt del codigo del curso
+            vista.getTxtCodigoCurso().setVisible(false);
+
+            ModeloCurso modeloCurso = new ModeloCurso();
+            List<Curso> listap = modeloCurso.listaCursoTabla();
+
+            listap.stream().forEach(c -> {
+
+                if (c.getCur_codigo() == Integer.parseInt(vista.getTblDlgjCurso().getValueAt(fila, 0).toString())) {
+
+                    vista.getTxtCodigoCurso().setText(String.valueOf(c.getCur_codigo()));
+                    vista.getTxtNombreCurso().setText(c.getCur_nombre());
+                    vista.getTxtPeriodoCurso().setText(c.getCur_periodo());
+
+                }
+            });
+
+            vista.getjDlgBuscarCurso().dispose();
+        }
+    }
+
+    public void buscarCurso() {
+
+        KeyListener eventoTeclado = new KeyListener() {//Crear un objeto de tipo keyListener(Es una interface) por lo tanto se debe implementar sus metodos abstractos
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+                ModeloCurso modeloCurso = new ModeloCurso();
+                vista.getTblDlgjCurso().setRowHeight(25);
+                DefaultTableModel estructuraTabla = (DefaultTableModel) vista.getTblDlgjCurso().getModel();
+                estructuraTabla.setRowCount(0);
+
+                List<Curso> listap = modeloCurso.buscarCurso(vista.getTxtBuscarCur().getText());
+
+                Holder<Integer> i = new Holder<>(0);
+
+                listap.stream().forEach(c -> {
+
+                    estructuraTabla.addRow(new Object[8]);
+
+                    vista.getTblDlgjCurso().setValueAt(c.getCur_codigo(), i.value, 0);
+                    vista.getTblDlgjCurso().setValueAt(c.getCur_nombre(), i.value, 1);
+                    vista.getTblDlgjCurso().setValueAt(c.getCur_periodo(), i.value, 2);
+
+                    i.value++;
+                });
+            }
+        };
+
+        vista.getTxtBuscarCur().addKeyListener(eventoTeclado); //"addKeyListener" es un metodo que se le tiene que pasar como argumento un objeto de tipo keyListener 
+    }
+
+    //Todo sobre Horario
+}
