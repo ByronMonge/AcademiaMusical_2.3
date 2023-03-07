715d97f22c098d2569a42d4a82208241f8681672
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-06 22:21:39 -0500

    Terminado

--- src/Modelo/ModeloMatricula.java
+++ src/Modelo/ModeloMatricula.java
@@ -1,7 +1,12 @@
 package Modelo;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ModeloMatricula extends Matricula {
 
@@ -15,8 +20,54 @@ public class ModeloMatricula extends Matricula {
     }
 
     public SQLException crearMatricula() {
-        String sql = "INSERT INTO matricula(mat_codamd, mat_codest, mat_codcurso, mat_fechamatri, mat_estado) VALUES (" + getMat_codAmd() + ", " + getMat_codEst() + ", " + getMat_codCur() + ", '" + getMat_fechamatri() + "','A';";
+
+        String sql = "INSERT INTO matricula(mat_codamd, mat_codest, mat_codcurso, mat_fechamatri, mat_estado) VALUES (" + getMat_codAmd() + ", " + getMat_codEst() + ", " + getMat_codCur() + ", '" + getMat_fechamatri() + "','A');";
 
         return conpg.accion(sql);
+    }
+
+    public SQLException modificarMatricula() {
+        String sql = "UPDATE matricula SET mat_codcurso = " + getMat_codCur() + " where mat_codigo = " + getMat_codigo() + ";";
+
+        return conpg.accion(sql);
+    }
+
+    public SQLException eliminarMatricula(int codigoMatricula) {
+        String sql = "UPDATE matricula SET mat_estado= 'I' where mat_codigo = " + codigoMatricula + ";";
+
+        return conpg.accion(sql);
+    }
+
+    public List<Matricula> listaMatriculaTabla() {
+        try {
+            List<Matricula> lista = new ArrayList<>();
+
+            String sql = "select * from matricula where mat_estado = 'A'";
+
+            ResultSet rs = conpg.consulta(sql);
+
+            while (rs.next()) {
+
+                Matricula matricula = new Matricula();
+
+                matricula.setMat_codigo(rs.getInt("mat_codigo"));
+                matricula.setMat_codAmd(rs.getInt("mat_codamd"));
+                matricula.setMat_codEst(rs.getInt("mat_codest"));
+                matricula.setMat_codCur(rs.getInt("mat_codcurso"));
+                matricula.setMat_fechamatri(rs.getDate("mat_fechamatri"));
+                matricula.setMat_estado(rs.getString("mat_estado"));
+
+                lista.add(matricula);
+            }
+
+            //Cierro la conexion a la BD
+            rs.close();
+            //Retorno la lista
+            return lista;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ModeloCurso.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 }
