d83b2f40a6182ce82b2516dec0f3d5b7a827b03c
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-09 23:50:19 -0500

    Ya esta terminado

--- src/Controlador/ControladorMatricula.java
+++ src/Controlador/ControladorMatricula.java
@@ -11,8 +11,20 @@ import Modelo.ModeloEstudiante;
 import Modelo.ModeloMatricula;
 import Vista.VistaMatricula;
 import Vista.VistaPrincipal;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import javax.swing.JOptionPane;
@@ -119,6 +131,9 @@ public class ControladorMatricula {
 
                 if (modelo.crearMatricula() == null) {
                     JOptionPane.showMessageDialog(null, "Se registro satisfactoriamente la matricula");
+                    cargarTablaDeMatriculas();
+
+                    crearPdf();//LLamo al metodo de crear pdf y enviar email
                 } else {
                     JOptionPane.showMessageDialog(null, "No se puedo registrar la matricula");
                 }
@@ -136,13 +151,12 @@ public class ControladorMatricula {
 
                 if (modelo.modificarMatricula() == null) {
                     JOptionPane.showMessageDialog(null, "La matricula se modifico exitosamente");
+                    cargarTablaDeMatriculas();
                 } else {
                     JOptionPane.showMessageDialog(null, "No se pudo modificar la matricula");
                 }
             }
         }
-
-        cargarTablaDeMatriculas();
     }
 
     int codigoMatricula;
@@ -314,6 +328,7 @@ public class ControladorMatricula {
                     vista.getTxtCodigoEstudiantematri().setText(String.valueOf(persona.getEst_codigo()));
                     vista.getTxtCedulaEstudiante().setText(persona.getPer_cedula());
                     vista.getTxtNombreYApellidoEstudiante().setText(persona.getPer_primernom() + " " + persona.getPer_apellidopater());
+                    vista.getTxtCorreoEstudiante().setText(persona.getPer_email());
 
                 }
             });
@@ -500,6 +515,7 @@ public class ControladorMatricula {
         vista.getTxtCodigoCurso().setVisible(false);
         vista.getTxtNombreCurso().setEditable(false);
         vista.getFechaDeMatricula().setEnabled(false);
+        vista.getTxtCorreoEstudiante().setVisible(false);
     }
 
     public void bloquearYVisibilidadModificar() {
@@ -513,6 +529,7 @@ public class ControladorMatricula {
         vista.getTxtNombreCurso().setEditable(false);
         vista.getFechaDeMatricula().setEnabled(false);
         vista.getBtnBuscarEstudiante().setVisible(false);
+        vista.getTxtCorreoEstudiante().setVisible(false);
     }
 
     public void limpiar() {
@@ -522,5 +539,154 @@ public class ControladorMatricula {
         vista.getTxtNombreYApellidoEstudiante().setText("");
         vista.getTxtCodigoCurso().setText("");
         vista.getTxtNombreCurso().setText("");
+    }
+
+    public void crearPdf() {
+        //Doy formato a la fecha para luego enviarla en el pdf
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaS = formato.format(vista.getFechaDeMatricula().getDate());
+
+        Email email = new Email(); //Creo un objeto de la clase "Email"
+
+        Document documento = new Document(); //Creo el documento en donde guardare la informacion
+
+        //Estas 2 lineas de codigo sirven solo si despues del usurio NO hay OneDrive
+        String usuario = System.getProperty("user.home");//Obtengo el usuario del equipo. Ej: C:\Users\monge
+
+        String ruta = usuario + "\\Documentos\\Matricula_" + vista.getTxtNombreYApellidoEstudiante().getText() + ".pdf"; //Concateno el "usuario" con la direccion en la que quiero que se guarde el documento (Ej:Documentos), luego coloco el nombre que tendra el documento (Ej:Factura...) y finalmente coloco la extension (Ej:pdf)
+        /*Ejemplo de "ruta": C:\Users\monge\Documentos\Factura_EdissonMonge.pdf
+         */
+
+        try {
+
+            PdfWriter.getInstance(documento, new FileOutputStream(ruta));//Le paso como parametro el "documento" en donde voy a escribir y la "ruta" en donde se va a guardar el pdf
+
+            documento.open();//Habro el documento para empezar a escribir en el mismo
+
+            //Agregar imagen al documento
+            try {
+
+                //com.itextpdf.text.Image imagen = com.itextpdf.text.Image.getInstance("src/Imagenes/luna roja.png");// Se usa de esta forma la libreria en caso de que salga un error al importarla (Pasa cuando se usa la misma clase de diferentes paquetes)
+                Image imagen = Image.getInstance("src/imagenes/estudiantes.png");//Esta es la forma normal. En esta linea se pasa la ruta de la imagen que se desea colocar en el pdf(En la linea 118 se hace lo mismo que aqui
+                imagen.scaleToFit(40, 1000);//(90 es el tamanio que tendra la imagen)
+                imagen.setAlignment(Chunk.ALIGN_TOP);//Es el lugar en donde se ubicara la imagen
+
+                documento.add(imagen);//Agrego la imagen al documento
+
+            } catch (BadElementException | IOException ex) {
+
+                System.out.println("Error Imagen: " + ex);
+            }
+
+            /*Agregar parrafos al documento*/
+            //Agregar parrafo 1
+            Paragraph parrafo = new Paragraph();
+            parrafo.setAlignment(Paragraph.ALIGN_CENTER);//Coloco la ubicacion del parrafo
+            parrafo.add("Registro de matrícula" + " \n \n \n ");//Ingreso el texto 
+            //parrafo.setFont(FontFactory.getFont("Magneto", 1000, Font.ITALIC, BaseColor.BLACK));
+            documento.add(parrafo);//Agrego el parrafo al documento
+
+            //Agregar parrafo 2
+            Paragraph parrafo2 = new Paragraph();
+            parrafo2.setAlignment(Paragraph.ALIGN_LEFT);
+            parrafo2.add("Datos del administrador:\n \n");
+            documento.add(parrafo2);
+
+            //Agregar parrafo 3
+            Paragraph parrafo3 = new Paragraph();
+            parrafo3.setAlignment(Paragraph.ALIGN_LEFT);
+            parrafo3.add("Cédula: " + vista.getTxtCedulaAdministrador().getText() + "                                                                   Nombre y apellido: " + vista.getTxtNombreYApellidoAdministrador().getText() + "\n \n \nInformación de la matrícula: \n \nFecha de matrícula: " + fechaS + "\n \n");
+            documento.add(parrafo3);
+
+            //Agregar los datos de la tabla al documento
+            PdfPTable tabla = new PdfPTable(4);//Creo una tabla con 4 columnas y coloco el titulo que tendra cada una
+            tabla.addCell("Número de matrícula");
+            tabla.addCell("Cédula del estudiante");
+            tabla.addCell("Nombre y apellido del estudiante");
+            tabla.addCell("Nombre del curso");
+
+            tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 0).toString());
+            tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 1).toString());
+            tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 2).toString());
+            tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 5).toString());
+
+            documento.add(tabla);//Agrego la tabla con los datos 
+
+            documento.close();//Cierro el documento
+
+            //Enviar pdf al correo
+            email.enviarEmail(vista.getTxtCorreoEstudiante().getText(), ruta, "Matricula_" + vista.getTxtNombreYApellidoEstudiante().getText() + ".pdf");
+        } catch (DocumentException | FileNotFoundException e) {
+
+            //System.out.println("Error sin OneDrive: " + e); //Esta excepcion me dara en caso de que no haya encontrado la ruta (Por lo general por que luego del usuario hay OneDrive. Ej:C:\Users\monge\OneDrive)
+            try {
+
+                //Esto se ejecutara SOLO SI ha existido una excepcion
+                //Esta "ruta" funciona si luego del "usuario" existe OneDrive y hace exactamente lo de la linea de codigo 104
+                ruta = usuario + "\\OneDrive\\Documentos\\Matricula_" + vista.getTxtNombreYApellidoEstudiante().getText() + ".pdf";
+                /*Ejemplo de "ruta":C:\Users\monge\OneDrive\Documentos\Factura_EdissonMonge.pdf*/
+
+                PdfWriter.getInstance(documento, new FileOutputStream(ruta));
+
+                documento.open();
+
+                //Agregar imagen al pdf
+                try {
+
+                    //com.itextpdf.text.Image imagen = com.itextpdf.text.Image.getInstance("src/Imagenes/luna roja.png");
+                    Image imagen = Image.getInstance("src/imagenes/estudiantes.png");
+                    imagen.scaleToFit(40, 1000);
+                    imagen.setAlignment(Chunk.ALIGN_RIGHT);
+
+                    documento.add(imagen);
+
+                } catch (BadElementException | IOException ex) {
+
+                    System.out.println("Error Imagen: " + ex);
+                }
+
+                /*Agregar parrafos al documento*/
+                //Agregar parrafo 1
+                Paragraph parrafo = new Paragraph();
+                parrafo.setAlignment(Paragraph.ALIGN_CENTER);//Coloco la ubicacion del parrafo
+                parrafo.add("Registro de matrícula" + " \n \n \n ");//Ingreso el texto 
+                //parrafo.setFont(FontFactory.getFont("Magneto", 1000, Font.ITALIC, BaseColor.BLACK));
+                documento.add(parrafo);//Agrego el parrafo al documento
+
+                //Agregar parrafo 2
+                Paragraph parrafo2 = new Paragraph();
+                parrafo2.setAlignment(Paragraph.ALIGN_LEFT);
+                parrafo2.add("Datos del administrador:\n \n");
+                documento.add(parrafo2);
+
+                //Agregar parrafo 3
+                Paragraph parrafo3 = new Paragraph();
+                parrafo3.setAlignment(Paragraph.ALIGN_LEFT);
+                parrafo3.add("Cédula: " + vista.getTxtCedulaAdministrador().getText() + "                                                                   Nombre y apellido: " + vista.getTxtNombreYApellidoAdministrador().getText() + "\n \n \nInformación de la matrícula: \n \nFecha de matrícula: " + fechaS + "\n \n");
+                documento.add(parrafo3);
+
+                //Agregar los datos de la tabla al documento
+                PdfPTable tabla = new PdfPTable(4);//Creo una tabla con 4 columnas y coloco el titulo que tendra cada una
+                tabla.addCell("Número de matrícula");
+                tabla.addCell("Cédula del estudiante");
+                tabla.addCell("Nombre y apellido del estudiante");
+                tabla.addCell("Nombre del curso");
+
+                tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 0).toString());
+                tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 1).toString());
+                tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 2).toString());
+                tabla.addCell(vista.getTblMatricula().getValueAt(vista.getTblMatricula().getRowCount() - 1, 5).toString());
+
+                documento.add(tabla);//Agrego la tabla con los datos 
+
+                documento.close();//Cierro el documento
+
+                //Enviar pdf al correo
+                email.enviarEmail(vista.getTxtCorreoEstudiante().getText(), ruta, "Matricula_" + vista.getTxtNombreYApellidoEstudiante().getText() + ".pdf");
+            } catch (DocumentException | FileNotFoundException ex) {
+
+                System.out.println("Error con OneDrive:" + ex); //Este mensaje me mostrara en caso haya un error tambien con el OneDrive
+            }
+        }
     }
 }
